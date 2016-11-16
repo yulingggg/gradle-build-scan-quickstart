7f93eff2216ad21b8e4d8fccaf2fda34dfd79efd
etiennestuder <etienne@gradle.com>
2016-11-16 12:55:16 -0800

    Demonstrate custom values

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -1,10 +1,16 @@
 package example;
 
 import com.google.common.base.Joiner;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.lang.Object;
 
 public class Example {
 
+    private static final Object OBJECT = new Object();
+
     public static String join(String... args) {
+        if (true) ; else;
         return Joiner.on(' ').join(args);
     }
 
