2c0c229ce568b6ce7ee938385ba5a9f4150238ba
yulingggg <chaiyuling@1utar.my>
2021-08-26 18:40:39 +0800

    modify example.java

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -4,7 +4,7 @@ import com.google.common.base.Joiner;
 
 public class Example {
 
-    private static String join(String... args) {
+    public static String join(String... args) {
         System.out.println("Hey");
         return Joiner.on(' ').join(args);
     }
