25852b76ef9188b38cc21221c9f83bdcd6eaa0b2
yulingggg <chaiyuling@1utar.my>
2021-08-26 16:58:40 +0800

    replace logger.log with system.out.print

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -5,7 +5,7 @@ import com.google.common.base.Joiner;
 private class Example {
 
     public static String join(String... args) {
-        logger.log("Hey");
+        System.out.println("Hey");
         return Joiner.on(' ').join(args);
     }
 
